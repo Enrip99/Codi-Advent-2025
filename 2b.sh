8c24c96eaa1aef294124e4d0d21f36bc5cb94991
enrip99 <enric.1999@hotmail.com>
2025-12-03 10:55:41 +0100

    eliminar codi antic comentat

--- 2b.sh
+++ 2b.sh
@@ -15,27 +15,6 @@ for i in ${array[@]}; do
 			suma=$(( suma + min ));
 			debug $min $suma
 		fi
-		#for (( segments = 2; segments <= ${#min}/2; ++segments )); do
-		#	if ! ((${#min} % $segments)); then
-		#		# Divideix net
-		#		debug "$min" "$segments" "->" "$((${#min} % $segments))"
-		#		mida=$(( ${#min} / $segments ));
-		#		debug MIDA: $mida
-		#		fallat=0;
-		#		for (( j = $mida; j < ${#min} && $fallat != 1 ; j += mida )); do
-		#			if ! [[ ${min:0:$mida} = ${min:j:$mida} ]]; then
-		#				debug ${min:0:$mida} VS ${min:j:$mida}
-		#				fallat=1;
-		#			fi
-		#		done
-		#		if [ $fallat -eq 0 ]; then
-		#			suma=$(( $suma + $min ));
-		#			debug Invàlid! Suma: $suma
-		#			(( ++min ));
-		#			continue;
-		#		fi
-		#	fi
-		#done
 		(( ++min ));
 	done
 done
